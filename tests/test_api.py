e735aab12a05a02b14d76eedb664c64783c3d968
Nafie <warrior259@gmail.com>
2021-11-23 09:51:25 +0300

    add test scripts

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,3 +3,17 @@ from fastapi.testclient import TestClient
 
 
 client = TestClient(app)
+
+def test_main_endpoint():
+    """Test main root"""
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+def test_404():
+    """ Test non-existed root"""
+
+    response = client.get("/any")
+
+    assert response.status_code == 404
