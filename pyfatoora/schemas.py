1285648ac334242a3940b87a45e4fa1902b35d75
Nafie <warrior259@gmail.com>
2021-12-09 12:13:05 +0300

    edit datatypes

--- pyfatoora/schemas.py
+++ pyfatoora/schemas.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
+from fastapi import Form
+import datetime
 
 class InvoiceData(BaseModel):
     seller_name: str 
-    tax_number: str
-    invoice_date: str
-    total_amount: str
-    tax_amount: str
+    tax_number: int
+    date: Optional[str] = str(datetime.datetime.now())
+    total: Optional[float] = 0.00
+    tax_amount: Optional[float] = total*0.15
